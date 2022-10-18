74c30b74774cfa6821d3519292293b3c5dbadc21
Zara <zama.ny1974@gmail.com>
2022-10-17 20:37:42 -0400

    started traking .idea folder by committing it

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,13 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("HelloTest");
+
+        //work1
+
+        //work2
+
+        //work3
+
+
     }
 }
